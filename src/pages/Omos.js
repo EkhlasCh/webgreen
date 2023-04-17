b8d7e3465b1c0648cc99ca7ad684a0e06428c013
EkhlasCh <101640098+EkhlasCh@users.noreply.github.com>
2023-04-17 18:04:24 +0200

    startet i omOs Siden

--- src/pages/Omos.js
+++ src/pages/Omos.js
@@ -1,6 +1,79 @@
+import ekhlas from "../images/omosteam/ekhlas.svg";
+
 export default function Omos() {
     return (
-        <p> dette er vores Om os </p>
-        )
+        
+        <div className="container">
+
+
+            <div className="row">
+
+
+                <div className="col">
+                    <img src={ekhlas} className="pic" alt="Ekhlas"></img>
+                    <div className="layer">
+                        <img  src={ekhlas} alt="" className="picOver"></img>
+                    </div>
+                    <div className="info">
+                        <h2 className="teamh2">Ekhlas</h2>
+                        <p className="teamP">Udvikler</p>
+                        <div className="icons">
+                            <i className=""></i>
+                            <i className=""></i>
+                            <i className=""></i>
+                        </div>
+                    </div>
+                </div>
+
+                
+
+                
+
+                <div className="col">
+                    <img src={ekhlas} className="pic" alt="Ekhlas"></img>
+                    <div className="layer">
+                        <img src={ekhlas} alt="" className="picOver"></img>
+                    </div>
+                    <div className="info">
+                        <h2 className="teamh2">Ekhlas</h2>
+                        <p className="teamP">Udvikler</p>
+                        <div className="icons">
+                            <i className=""></i>
+                            <i className=""></i>
+                            <i className=""></i>
+                        </div>
+                    </div>
+                </div>
+
+
+                
+
+
+
+                <div className="col">
+                    <img src={ekhlas} className="pic" alt="Ekhlas"></img>
+                    <div className="layer">
+                        <img  src={ekhlas} alt="" className="picOver"></img>
+                    </div>
+                    <div className="info">
+                        <h2 className="teamh2">Ekhlas</h2>
+                        <p className="teamP">Udvikler</p>
+                        <div className="icons">
+                            <i className=""></i>
+                            <i className=""></i>
+                            <i className=""></i>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div> 
+            
+        
+
+
+    )
 
 }

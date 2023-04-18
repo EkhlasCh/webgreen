579fb291b984b59df948f01c0253a7f8970c56b6
EkhlasCh <101640098+EkhlasCh@users.noreply.github.com>
2023-04-18 13:56:27 +0200

    Kontakt siden færdig :)

--- src/pages/Kontakt.js
+++ src/pages/Kontakt.js
@@ -1,16 +1,61 @@
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faEnvelopeOpenText, faHeadset, faPhoneVolume } from "@fortawesome/free-solid-svg-icons";
+
 export default function Kontakt() {
     return (
-        <p> dette er vores kontakt
-            ,æ,aæ,mælamdcø
-            d,ææmdcæncl
-            ndflknlsnclksnlknc
-            lklcnlndclkn
-            
-        </p>
 
+
+       
+
+        
+        <section className="kontaktside-container">
+            <div className="kontakt-side-banner">
+
+                <h1 className="kontak-overskrift">Her kan du kontakte os</h1>
+
+                <h5 className="kontakt-spg">Har du spørgsmål eller brug for hjælp? </h5>
+                <p>
+                    Tøv ikke med at kontakte os, vi sidder klar til at hjælpe dig 
+                    med alt fra at booke møde med en konsulent, undervisning 
+                    af dine medarbejdere, samt service og support til dit 
+                    Green Wave website.
+                </p>
+            </div>
+
+            
+
+            <section className="KontakLinks">
+             
+
+                <div className="k-søjle support">
+                    <FontAwesomeIcon className="kontakSide-Icon" icon={faEnvelopeOpenText} />
+                    <p className="værdierP"><b>info@gwd.dk</b></p>
+                </div>
+
+                <div className="k-søjle telefon">
+                    <FontAwesomeIcon className="kontakSide-Icon" icon={faPhoneVolume} />
+                    <h6>Hovedkontoret</h6>
+                    <p className="værdierP"><b>+45 26 16 16 16</b></p>
+                </div>
+
+                <div className="k-søjle mail">
+                    <FontAwesomeIcon className="kontakSide-Icon" icon={faHeadset} />
+                    <p className="værdierP"><b>support@gwd.dk</b> </p>
+                </div>
+               
+            </section>
+
+
+        </section>
+
+
+
+
+   
 
 
         
-        )
+    )
 
 }
